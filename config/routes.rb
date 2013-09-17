9627b78d17dd63f00682bdf0fffb05586aaf1b94
rraciop <ryanracioppo@gmail.com>
2013-09-17 00:01:39 -0400

    Added styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,6 @@
 Rainforest::Application.routes.draw do
 
-  get "reviews/show"
 
-  get "reviews/new"
-
-  get "reviews/edit"
-
-  get "sessions/new"
-
-  get "sessions/create"
-
-  get "sessions/destroy"
-
-  get "users/new"
-
-  get "users/create"
 
 resources :products
 resources :users, :only => [:new, :create]
